19fb8dadad56e73d0fd74749e6813bd1fb6691db
Enoki <enokiun@gmail.com>
2022-11-03 23:26:19 +0300

    refactor: change Data back to web::Data

--- src/info_endpoints.rs
+++ src/info_endpoints.rs
@@ -1,18 +1,17 @@
 use crate::board;
 use crate::AppState;
-use actix_web::web::Data;
-use actix_web::{get, HttpRequest, HttpResponse, Responder, Result};
+use actix_web::{get, web, HttpRequest, HttpResponse, Responder, Result};
 use serde_json::json;
 
 #[get("/board")]
-async fn get_board(state: Data<AppState>) -> impl Responder {
+async fn get_board(state: web::Data<AppState>) -> impl Responder {
     let board: Vec<Vec<board::Tile>> = state.board.lock().unwrap().to_vec();
 
     HttpResponse::Ok().json(board)
 }
 
 #[get("/@me/id")]
-async fn get_current_user_id(request: HttpRequest, state: Data<AppState>) -> impl Responder {
+async fn get_current_user_id(request: HttpRequest, state: web::Data<AppState>) -> impl Responder {
     let user_id = match require_authentication(request, state) {
         Ok(id) => id,
         Err(resp) => return resp,
@@ -23,7 +22,7 @@ async fn get_current_user_id(request: HttpRequest, state: Data<AppState>) -> imp
 
 fn require_authentication(
     request: HttpRequest,
-    state: Data<AppState>,
+    state: web::Data<AppState>,
 ) -> Result<String, HttpResponse> {
     if let Some(authentication) = request.headers().get("authentication") {
         use itsdangerous::{default_builder, Signer};
